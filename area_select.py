af3beae65c909dc0272440b5fb40c03946d332e3
lfrothin <151072142+lfrothin@users.noreply.github.com>
2023-11-23 14:03:14 -0500

    Update area_select.py
    debugged previous version

--- area_select.py
+++ area_select.py
@@ -1,25 +1,25 @@
 import numpy as np
 import cv2
-img = cv2.imread('cafe.jpg')
-drawing = True
-shapes = []
-curr_shape_index = []
+
+img = cv2.imread('cafe.jpg') #import example image
+
+shapes = [] #list to store all drawn areas
+curr_shape_index = [] #list to store the indices of the polygon currently drawing
 def draw_polygon(event, x, y, flags, params):
-    global drawing, shapes, curr_shape_index
+    global shapes, curr_shape_index
     color = (0, 0, 255) #red
     thickness = 5
-    if event == cv2.EVENT_LBUTTONDOWN:
-        if drawing is False: drawing = True
-        curr_shape_index.append((x, y)) #assumes list is empty initially
-    elif event == cv2.EVENT_LBUTTONDBLCLK:
-        drawing = False
+    if event == cv2.EVENT_LBUTTONDOWN: #for all points except last, left click
+        curr_shape_index.append((x, y))
+    elif event == cv2.EVENT_MBUTTONDOWN: #middle click to denote last vertex of polygon
+        curr_shape_index.append((x, y))
         isClosed = True
         pts = np.array(curr_shape_index, np.int32)
 
-        cv2.polylines(img, [pts], 
-                      isClosed, color, thickness)
-        shapes.append(curr_shape_index.copy())
-        curr_shape_index = []
+        cv2.polylines(img, [pts], isClosed, color, thickness) #draw polygon on img
+        shapes.append(curr_shape_index.copy()) #save polygon
+        curr_shape_index = [] #reset current indices list
+    
 
 cv2.namedWindow("Image")
 cv2.setMouseCallback("Image", draw_polygon)
@@ -31,6 +31,7 @@ while True:
         cv2.imwrite('output.png', img)
         print(f'number of areas = {len(shapes)}')
         print(shapes) #print coordinates of areas
+        #print(type(shapes))
         break    
 
 cv2.destroyAllWindows()
