2ce85b2a1fc8fc4959170414717fac4b374bec2e
SebastianLevy <81613373+SebastianLevy@users.noreply.github.com>
2023-11-30 13:09:18 -0500

    runs on one image, ouptuts names of areas

--- main.py
+++ main.py
@@ -2,89 +2,76 @@ import cv2
 import pandas as pd
 import numpy as np
 from ultralytics import YOLO
+import json
 
+# Path to your JSON file
+json_file_path = 'areas.json'
+
+# Open and read the JSON file
+with open(json_file_path, 'r') as file:
+    areas = json.load(file)
+    areas = {tuple(value): key for key, value in areas.items()}
 
 model=YOLO('yolov8s.pt')
 
-sample_vids = ['CCTV_720p.mov', 'CCTV_1080p.mov', 'peopleCount.mp4']
-
-vid = 'SampleVideos/' + sample_vids[1]
-
-area1=[(312,388),(289,390),(474,469),(497,462)]
-
-area2=[(279,392),(250,397),(423,477),(454,469)]
-def RGB(event, x, y, flags, param):
-    if event == cv2.EVENT_MOUSEMOVE :  
-        colorsBGR = [x, y]
-        print(colorsBGR)
-        
-
 cv2.namedWindow('RGB')
-cv2.setMouseCallback('RGB', RGB)
-
-cap=cv2.VideoCapture(vid)
-
 
 my_file = open("coco.txt", "r")
 data = my_file.read()
 class_list = data.split("\n") 
-#print(class_list)
 
 trackers = []
 count=0
 
-while True:    
-    ret, frame = cap.read()
-    if not ret:
-        break
-    count += 1
-    if count % 2 != 0:
-        continue
-    frame = cv2.resize(frame, (1020, 500))
+frame = cv2.resize(cv2.imread('bar.jpg'), (1020, 500))
+mask = cv2.imread('mask.png')
 
-    results = model.predict(frame)
 
-    # Accessing the boxes, confidences, and class IDs
-    boxes = results[0].boxes.xyxy
-    confidences = results[0].boxes.conf
-    class_ids = results[0].boxes.cls
+results = model.predict(frame)
 
-    # Iterating through detections
-    for i in range(len(boxes)):
-        box = boxes[i]
-        confidence = confidences[i]
-        class_id = class_ids[i]
+# Accessing the boxes, confidences, and class IDs
+boxes = results[0].boxes.xyxy
+confidences = results[0].boxes.conf
+class_ids = results[0].boxes.cls
+converted_boxes = []
+for box in boxes:
+    x1, y1, x2, y2 = map(int, box[:4])
+    width, height = x2 - x1, y2 - y1
+    x, y = x1, y1  # Top-left corner
+    converted_boxes.append([x, y, width, height])
 
-        # Convert class_id to a Python integer
-        class_id = int(class_id)  # or class_id.item() if class_id is a single-element tensor
+confidence_list = [float(conf) for conf in confidences]
 
-        x1, y1, x2, y2 = map(int, box[:4])
-        xc = int((x1 + x2) / 2)
-        yc = y2
+indices = cv2.dnn.NMSBoxes(converted_boxes, confidence_list, 0.5, 0.4)
 
-        c = class_list[class_id]
+# Iterating through detections
+for i in indices:
+    box = boxes[i]
+    confidence = confidences[i]
+    class_id = class_ids[i]
 
-        if 'person' in c:
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(frame, str(c), (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
-            cv2.circle(frame, (x1, y1), 5, (0, 0, 255), -1)  # Top-left in red
-            cv2.circle(frame, (x2, y1), 5, (0, 255, 0), -1)  # Top-right in green
-            cv2.circle(frame, (x1, y2), 5, (255, 0, 0), -1)  # Bottom-left in blue
-            cv2.circle(frame, (x2, y2), 5, (255, 255, 0), -1)  # Bottom-right in cyan
-            cv2.circle(frame, (xc, yc), 5, (255, 0, 255), -1)  # Bottom-Midpoint in pink
-            
-            
+    # Convert class_id to a Python integer
+    class_id = int(class_id)  # or class_id.item() if class_id is a single-element tensor
+
+    x1, y1, x2, y2 = map(int, box[:4])
+    xc = int((x1 + x2) / 2)
+    yc = y2
+
+    c = class_list[class_id]
+
+    colour = tuple(int(val) for val in mask[yc, xc])
+
+    if 'person' in c:
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(frame, str(c), (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+        cv2.circle(frame, (xc, yc), 5, colour, -1)
+
+    area_col = mask[yc, xc]
+    area_col_tuple = tuple(area_col)
+
+    # Find the area name by color
+    area_name = areas.get(area_col_tuple, "no color")
+    print(area_name)
         
-    cv2.polylines(frame,[np.array(area1,np.int32)],True,(255,0,0),2)
-    cv2.putText(frame,str('1'),(504,471),cv2.FONT_HERSHEY_COMPLEX,(0.5),(0,0,0),1)
-
-    cv2.polylines(frame,[np.array(area2,np.int32)],True,(255,0,0),2)
-    cv2.putText(frame,str('2'),(466,485),cv2.FONT_HERSHEY_COMPLEX,(0.5),(0,0,0),1)
-
-    cv2.imshow("RGB", frame)
-    if cv2.waitKey(1)&0xFF==27:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
+cv2.imshow("RGB", frame)
+cv2.waitKey(0)
