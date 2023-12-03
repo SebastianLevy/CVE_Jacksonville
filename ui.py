19b877cc731ff6880e5073a9d57a92c8b9656c92
lfrothin <151072142+lfrothin@users.noreply.github.com>
2023-12-03 13:28:09 -0500

    Update ui.py
    debugged, now can draw green rectangle

--- ui.py
+++ ui.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-global add_param, slide
+#global add_param, slide
 
-slide = None
+slide = False
 # base image
 cafe = cv2.imread('cafe.jpg')
 height_base, width_base, channels = cafe.shape
@@ -29,11 +29,13 @@ print(roi_x_min)
 print(roi_x_max)
 
 def on_mouse_click(event, x, y, flags, param):
+    global slide, concat
     if event == cv2.EVENT_LBUTTONDOWN: 
         print(f"Left mouse button clicked at ({x}, {y})")
         
         if roi_x_min <= x <= roi_x_max and roi_y_min <= y <= roi_y_max:
             if param == 'first_sidebar':
+                slide = True
                 print('first_sidebar')
                 image_path = 'instructions.png'
                 image = cv2.imread(image_path)
@@ -42,32 +44,32 @@ def on_mouse_click(event, x, y, flags, param):
                 # adjust this code so that the original photo with the polygons is still there
                 # or can draw polygons at the end and show that image
                 concat = np.concatenate((cafe, resized_image), axis=1)
-                cv2.imshow('SmartCrowd', concat)
-                slide = True
-                #concat_im = 'instructions'
-
+        
             elif param == 'instructions':
                 print('instruction')
-                top_left = (roi_x_min, 2/3 * height_total)
-                bottom_right = (roi_x_max, 4/5 * height_total)
+                top_left = (roi_x_min, int(2/3 * height_total))
+                bottom_right = (roi_x_max, int(4/5 * height_total))
                 color = (0, 255, 0)
                 thickness = 2
                 cv2.rectangle(concat, top_left, bottom_right, color, thickness)
 
-
-cv2.imshow('SmartCrowd', concat)
-
+cv2.namedWindow("SmartCrowd")
+add_param = 'first_sidebar'
+cv2.setMouseCallback('SmartCrowd', on_mouse_click, param=add_param)
+called = False
+    
 # Set the callback function for mouse events
 while True:
-    if slide == None:
-        add_param = 'first_sidebar'
-        cv2.setMouseCallback('SmartCrowd', lambda event, x, y, flags, param: on_mouse_click(event, x, y, flags, add_param))
-
-    elif slide == True and add_param == 'first_sidebar':
+    cv2.imshow('SmartCrowd', concat)
+    #print(slide, add_param, called)
+    if slide is True and add_param == 'first_sidebar' and called is False:
         add_param = 'instructions'
-        cv2.setMouseCallback('SmartCrowd', lambda event, x, y, flags, param: on_mouse_click(event, x, y, flags, add_param))
-
+        called = True
+        cv2.setMouseCallback('SmartCrowd', on_mouse_click, param=add_param)
+    
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
 # logic for clicking in the click to proceed
 #if concat_im == 'first_sidebar' and 
-cv2.waitKey(0)
+
 cv2.destroyAllWindows()
